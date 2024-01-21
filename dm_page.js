88d3f14667990b5eed2287f0f1e9b05c8d8a68f0
Abenezer Workineh <abenezer037005@gmail.com>
2024-01-21 13:26:08 +0300

    cleaned up dm page and added messaging functionality

--- dm_page.js
+++ dm_page.js
@@ -1,5 +1,27 @@
-$(document).ready(function(){
-	$('#action_menu_btn').click(function(){
-		$('.action_menu').toggle();
-	});
+const sidebar = document.getElementsByClassName("sidebar");
+const menubtn = document.getElementById("toggle-menu");
+const chat = document.getElementsByClassName("chat");
+const sendbtn = document.getElementById("send");
+const text = document.getElementById("text-input");
+
+function makeChatElement(s) {
+	res = `<div class="chat-text-bubble sent"> ${s} </div>`;
+	return res;
+}
+
+sidebar[0].style.display = "none";
+menubtn.addEventListener("click", () => {
+	console.log("hello");
+	if (sidebar[0].style.display === "none") {
+		sidebar[0].style.display = "block";
+	} else {
+		sidebar[0].style.display = "none";
+	}
+});
+
+sendbtn.addEventListener("click", () => {
+	console.log(chat);
+	event.preventDefault();
+	chat[0].innerHTML += makeChatElement(text.value);
+	text.value = "";
 });
