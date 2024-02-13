48390db46f8f93a048f52c708620de62529adcda
Abigail F.Hassen <bigidovi@gmail.com>
2024-02-13 09:51:19 +0300

    edit layouts

--- script.js
+++ script.js
@@ -20,13 +20,26 @@ document.querySelectorAll('#my-profile-picture').forEach(AllProfile =>{
 document.querySelectorAll('.close').forEach(AllProfile => {
     AllProfile.addEventListener('click', ()=>{
         document.querySelector('.popup').style.display="none"
-
     })
 })
 
-document.querySelector('#profile-upload').addEventListener('change', () =>{
-    document.querySelectorAll('#my-profile-picture img').forEach(AllMyProfileImg=>{
-        AllMyProfileImg.src= URL.createObjectURL(document.querySelector('#profile-upload').files[0])
-    })
-})
+document.querySelectorAll('#profile-upload').forEach(input => {
+    input.addEventListener('change', () => {
+        document.querySelectorAll('#my-profile-picture img').forEach(AllMyProfileImg => {
+            AllMyProfileImg.src = URL.createObjectURL(input.files[0]);
+        });
+    });
+});
 
+
+// // add post
+// document.querySelector('#crat-lg').addEventListener('click', () => {
+//     document.querySelector('.add-post-popup').style.display = 'flex';
+// });
+
+
+// document.querySelectorAll('#feed-pic-upload').forEach(input => {
+//     input.addEventListener('change', () => {
+//         document.querySelector('#postImg').src = URL.createObjectURL(input.files[0]);
+//     });
+// });
